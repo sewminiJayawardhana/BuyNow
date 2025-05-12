ff4a9f155605c3d490bc13281b4f61a6752ab4d7
Sewmini <sewuminijayawardana23@gmail.com>
2025-05-12 12:52:41 +0530

    updated ProductCard and ProductDetails components with minor changes`

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
+import {assets} from "../assets/assets";
+import ProductCard from "../components/ProductCard";
 
 const ProductDetails = () => {
 
     const { products,navigate,currency,addToCart } = useAppContext();
     const { id } = useParams();
-    const [relatedProducts, setRelatedProducts] = useState(product.images[0]);
+    const [relatedProducts, setRelatedProducts] = useState([]);
     const [thumbnail, setThumbnail] = useState(null);
 
     const product = products.find((item) => item._id === id);
+
 
     useEffect(() => {
         if (products.length>0) {
@@ -20,7 +23,7 @@ const ProductDetails = () => {
     },[products])
 
     useEffect(() => {
-        setThumbnail(product.images[0] ? product.images[0] : null)
+        setThumbnail(product?.image[0] ? product.image[0] : null)
     },[product])
 
     return product && (
@@ -28,8 +31,8 @@ const ProductDetails = () => {
             <p>
                 <Link to={"/"}>Home</Link> /
                 <Link to={"/products"}> Products</Link> /
-                <Link to={"/products/${product.category.toLowerCase()}"}> {product.category}</Link> /
-                <span className="text-indigo-500"> {product.name}</span>
+                <Link to={`/products/${product.category.toLowerCase()}`}> {product.category}</Link> /
+                <span className="text-primary"> {product.name}</span>
             </p>
 
             <div className="flex flex-col md:flex-row gap-16 mt-4">
@@ -52,18 +55,15 @@ const ProductDetails = () => {
 
                     <div className="flex items-center gap-0.5 mt-1">
                         {Array(5).fill('').map((_, i) => (
-                            product.rating > i ? (
-                               <img src={i<4 ? assets.star_icon : assets.star_dull_icon} alt="star"  />
-                            ) 
-                                
-                            )
+                             <img src={i<4 ? assets.star_icon : assets.star_dull_icon} alt="star"  className="md:w-4 w-3.5"/>
+            
                         ))}
-                        <p className="text-base ml-2">({product.rating})</p>
+                        <p className="text-base ml-2">(4)</p>
                     </div>
 
                     <div className="mt-6">
-                        <p className="text-gray-500/70 line-through">MRP: ${product.price}</p>
-                        <p className="text-2xl font-medium">MRP: ${product.offerPrice}</p>
+                        <p className="text-gray-500/70 line-through">MRP:{currency}{product.price}</p>
+                        <p className="text-2xl font-medium">MRP:{currency}{product.offerPrice}</p>
                         <span className="text-gray-500/70">(inclusive of all taxes)</span>
                     </div>
 
@@ -75,14 +75,28 @@ const ProductDetails = () => {
                     </ul>
 
                     <div className="flex items-center mt-10 gap-4 text-base">
-                        <button className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition" >
+                        <button onClick={()=>addToCart(product._id)}
+                        className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition" >
                             Add to Cart
                         </button>
-                        <button className="w-full py-3.5 cursor-pointer font-medium bg-indigo-500 text-white hover:bg-indigo-600 transition" >
+                        <button onClick={()=>{addToCart(product._id);navigate("/cart")}} className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition" >
                             Buy now
                         </button>
                     </div>
                 </div>
+            </div>
+            <div className="flex flex-col items-center mt-20">
+                <div className="flex flex-col items-center w-max">
+                    <p>Related Products</p>
+                    <div className="w-20 h-0.5 bg-primary rounded-full mt-2"> </div>
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+                    {relatedProducts.filter((product)=>product.inStock).map((product,index)=>(
+                        <ProductCard key={index} product={product} />
+                    ))}
+                </div>
+                <button onClick={()=>{navigate('/products'); scrollTo(0,0)}} 
+                className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition">See More </button>
             </div>
         </div>
     );
