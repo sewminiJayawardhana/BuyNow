12c5ce54c328fae93af2fcac975b4b5aa6df9f26
Sewmini <sewuminijayawardana23@gmail.com>
2025-05-18 11:34:17 +0530

    `Added AddAddress page and route to App component and updated Cart component to accept product ID in removeFromCart function`

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -72,8 +72,8 @@ const Cart = () => {
                             </div>
                         </div>
                         <p className="text-center">{currency}{product.offerPrice * product.quantity}</p>
-                        <button onClick={()=>removeFromCart() }className="cursor-pointer mx-auto">
-                            <img src={assets.refresh_icon} alt="remove" className="inline-block w-6 h-6" />
+                        <button onClick={()=>removeFromCart(product._id) }className="cursor-pointer mx-auto">
+                            <img src={assets.remove_icon} alt="remove" className="inline-block w-6 h-6" />
                         </button>
                     </div>)
                 )}
