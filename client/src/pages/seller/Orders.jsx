19ed14898fb1c782a1d4436f89c758dea86455c1
Sewmini <sewuminijayawardana23@gmail.com>
2025-05-27 12:13:16 +0530

    Added useState hook and updated JSX structure in Orders component

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState} from 'react'
 import { useAppContext } from '../../context/AppContext'
 import { assets, dummyOrders } from '../../assets/assets'
 
@@ -17,17 +17,17 @@ const Orders = () => {
     
         
   return (
-    <div>
+    
     <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll">
         <div className="md:p-10 p-4 space-y-4">
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
-                <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
-                    <div className="flex gap-5">
-                        <img className="w-12 h-12 object-cover opacity-60" src={assets.box_icon} alt="boxIcon" />
+                <div key={index} className="flex flex-col md:items-center md:flex-row gap-5 justify-between p-5 max-w-4xl rounded-md border border-gray-300 ">
+                    <div className="flex gap-5 max-w-80">
+                        <img className="w-12 h-12 object-cover" src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
-                                <div key={index} className="flex flex-col justify-center">
+                                <div key={index} className="flex flex-col">
                                     <p className="font-medium">
                                         {item.product.name} 
                                         <span className="text-primary"> x {item.quantity}</span>
@@ -41,13 +41,16 @@ const Orders = () => {
                         <p className='text-black/80'>
                         {order.address.firstName}{order.address.lastName}</p>
                         <p>{order.address.street}, {order.address.city}, {order.address.state},{order.address.zipcode}, {order.address.country}</p>
+
+                        <p></p>
+                        <p>{order.address.phone}</p>
                     </div>
 
-                    <p className="font-medium text-base my-auto text-black/70">${order.amount}</p>
+                    <p className="font-medium text-lg my-auto">{currency}{order.amount}</p>
 
                     <div className="flex flex-col text-sm">
                         <p>Method: {order.paymentType}</p>
-                        <p>Date: {order.orderDate}</p>
+                        <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
                         <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
                     </div>
                 </div>
