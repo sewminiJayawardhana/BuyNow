44827bc0ad703dd79d3198d25667b8f25573acef
Sewmini <sewuminijayawardana23@gmail.com>
2025-05-31 17:39:08 +0530

    `Fixed MongoDB URI and updated user login functionality in db.js and userController.js`

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try{
         mongoose.connection.on('connected',()=> console.log("Database Connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/buynow`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/BuyNow`)
     }catch(error){
         console.error(error.message);
     }
