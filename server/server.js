242727d0406f3278c99e92527159470c3f426475
Sewmini <sewuminijayawardana23@gmail.com>
2025-06-04 00:05:18 +0530

    Updated product controller and server to handle image uploads and product CRUD operations.

--- server/server.js
+++ server/server.js
@@ -5,13 +5,15 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
 
 
 const app=express();
 const port=process.env.PORT || 4000;
 
 await connectDB()
-
+await connectCloudinary()
 
 //Allow multiple origins
 const allowedOrigins =['http://localhost:5173']
@@ -26,6 +28,7 @@ app.use(express.json());
 app.get('/', (req, res) => res.send("API is working"));
 app.use('/api/user',userRouter)
 app.use('/api/seller',sellerRouter)
+app.use('/api/product',productRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
