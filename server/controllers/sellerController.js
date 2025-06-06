fd2574b671331cad34ba486745bc92a4f4433fa1
Sewmini <sewuminijayawardana23@gmail.com>
2025-06-06 23:24:49 +0530

    `Fixed import and function call for jwt, corrected variable names and error handling in sellerLogin function.`

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+
 //Login Seller : /api/seller/login
 
 export const sellerLogin =async(req,res)=>{
@@ -7,7 +8,7 @@ export const sellerLogin =async(req,res)=>{
         const{email,password}=req.body;
 
     if(password===process.env.SELLER_PASSWORD && email===process.env.SELLER_EMAIL){
-        const token=JsonWebTokenError.sign({email},process.env.JWT_SECRET,{expireIn:'7d'});
+        const token=jwt.sign({email},process.env.JWT_SECRET,{expireIn:'7d'});
 
         res.cookie('sellerToken',token, {
             httpOnly:true, 
@@ -15,13 +16,13 @@ export const sellerLogin =async(req,res)=>{
             sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
             maxAge:7 * 24 * 60 * 60 * 1000 , 
         });
-        return res.json({successs:true,message:"Logged In"});
+        return res.json({success:true,message:"Logged In"});
     }else{
-        return res.json({successs:false,message:"Invalid Credentials"});
+        return res.json({success:false,message:"Invalid Credentials"});
     }
     } catch (error) {
         console.log(error.message);
-        res.json({successs:false,message:error.message});
+        res.json({success:false,message:error.message});
     }
 }
 //Seller isAuth :/api/seller/is-auth
