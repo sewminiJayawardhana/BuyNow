ebe72a16b75cfa8179f35b866f89362747c10a13
Sewmini <sewuminijayawardana23@gmail.com>
2025-06-21 22:42:57 +0530

    `Added Stripe order placement logic to Cart component`

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -57,7 +57,21 @@ const Cart = () => {
                     }else{
                     toast.error(data.message)
                 }
+            }else{
+                //Place order with Stripe
+                const {data}=await axios.post('/api/order/Stripe',{
+                    userId: user._id,
+                   items: cartArray.map(item => ({product:item._id,quantity:item.quantity})),
+                   address: selectedAddress._id
+                })
+                if(data.success){
+                    window.location.replace(data.url) 
+                }else{
+                    toast.error(data.message)
+                }
+
             }
+            
         } catch (error) {
             toast.error(error.message)
             
