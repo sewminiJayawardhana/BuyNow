7cd5bb61f00b1e382ae37324ea33171cc28d2e4c
Sewmini <sewuminijayawardana23@gmail.com>
2025-07-21 00:23:06 +0530

    `Removed 'flex-row' display class from BottomBanner component`

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -3,7 +3,7 @@ import { assets, features } from "../assets/assets";
 
 const BottomBanner = () => {
   return (
-    <div className="relative mt-24 display flex-row">
+    <div className="relative mt-24 ">
       <img
         src={assets.bottom_banner_image}
         alt="banner"
@@ -16,7 +16,7 @@ const BottomBanner = () => {
         className="w-full md:hidden"
       />
 
-      <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
+      <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24 ">
         {" "}
       </div>
       <h1 className="text-2xl md:text-3xl font-semibold text-primary mb-6">
@@ -41,3 +41,5 @@ const BottomBanner = () => {
 };
 
 export default BottomBanner;
+
+
